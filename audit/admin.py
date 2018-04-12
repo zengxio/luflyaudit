77ebb191dedbf1da4dae9cf607bfebcf8cf57904
zxy-edu <1264983873@qq.com>
2018-04-12 10:39:49 +0800

    'test9'

--- audit/admin.py
+++ audit/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from audit import models
+
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ['session','cmd','date']
+    list_filter = ['date','session']
+
+class SessionLogAdmin(admin.ModelAdmin):
+    list_display = ['id','account','host_user_bind','start_date','end_date']
+    list_filter = ['start_date','account']
+
 # Register your models here.
 admin.site.register(models.Host)
 admin.site.register(models.HostUserBind)
 admin.site.register(models.HostGroup)
 admin.site.register(models.HostUser)
 admin.site.register(models.Account)
-admin.site.register(models.AuditLog)
+admin.site.register(models.AuditLog,AuditLogAdmin)
 admin.site.register(models.IDC)
-admin.site.register(models.SessionLog)
+admin.site.register(models.SessionLog,SessionLogAdmin)
+
 
---
+++ audit/backend/ssh_interactive.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
+#
+# This file is part of paramiko.
+#
+# Paramiko is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+
+import base64
+from binascii import hexlify
+import getpass
+import os
+import select
+import socket
+import sys
+import time
+import traceback
+from paramiko.py3compat import input
+
+import paramiko
+from audit import models
+try:
+    import interactive
+except ImportError:
+    from . import interactive
+
+
+
+def manual_auth(t,username,password):
+    t.auth_password(username, password)
+
+# now connect
+def ssh_session(bind_host_user,user_obj):
+    hostname=bind_host_user.host.ip_addr
+    port=bind_host_user.host.port
+    username=bind_host_user.host_user.username
+    password=bind_host_user.host_user.password
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((hostname, port))
+    except Exception as e:
+        print('*** Connect failed: ' + str(e))
+        traceback.print_exc()
+        sys.exit(1)
+
+    try:
+        t = paramiko.Transport(sock)
+        try:
+            t.start_client()
+        except paramiko.SSHException:
+            print('*** SSH negotiation failed.')
+            sys.exit(1)
+
+        try:
+            keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+        except IOError:
+            try:
+                keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+            except IOError:
+                print('*** Unable to open host keys file')
+                keys = {}
+
+        # check server's host key -- this is important.
+        key = t.get_remote_server_key()
+        if hostname not in keys:
+            print('*** WARNING: Unknown host key!')
+        elif key.get_name() not in keys[hostname]:
+            print('*** WARNING: Unknown host key!')
+        elif keys[hostname][key.get_name()] != key:
+            print('*** WARNING: Host key has changed!!!')
+            sys.exit(1)
+        else:
+            print('*** Host key OK.')
+
+
+        # agent_auth(t, username)
+        if not t.is_authenticated():
+            manual_auth(t,username,password)
+        if not t.is_authenticated():
+            print('*** Authentication failed. :(')
+            t.close()
+            sys.exit(1)
+
+        chan = t.open_session()
+        chan.get_pty()
+        chan.invoke_shell()
+        print('*** Here we go!\n')
+        session_obj=models.SessionLog.objects.create(account=user_obj.account,
+                                                     host_user_bind=bind_host_user,)
+        interactive.interactive_shell(chan,session_obj)
+        chan.close()
+        t.close()
+
+    except Exception as e:
+        print('*** Caught exception: ' + str(e.__class__) + ': ' + str(e))
+        traceback.print_exc()
+        try:
+            t.close()
+        except:
+            pass
+        sys.exit(1)
+
+
